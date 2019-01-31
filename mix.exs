7ebec51abd307a1c37db0501bd733a8aa38aa1d8
SimonLab <simon.labondance@gmail.com>
2019-01-31 12:43:40 +0000

    bump version to 0.1.1, #11

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Base58Encode.MixProject do
       name: "b58",
       description: description(),
       package: package(),
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
