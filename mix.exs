97b46bd30f2bee5289b041ba58e79f301e976c6d
nelsonic <nelson+github@dwyl.io>
2020-04-25 23:17:47 +0100

    update dependencies

--- mix.exs
+++ mix.exs
@@ -30,23 +30,23 @@ defmodule Base58.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:excoveralls, "~> 0.10", only: :test},
+      {:excoveralls, "~> 0.12.3", only: :test},
       {:basefiftyeight, "~> 0.1.0", only: :test},
-      {:stream_data, "~> 0.1", only: :test},
+      {:stream_data, "~> 0.4.3", only: :test},
       {:ex_doc, "~> 0.19.3", only: :dev}
     ]
   end
 
   defp description() do
-    "`B58` lets you `encode` an Elixir binary to base58 and `decode` a base58 string."
+    "B58 lets you encode an Elixir binary to base58 and decode a base58 string."
   end
 
   defp package() do
     [
-      name: "exbase58",
+      name: "b58",
       licenses: ["GNU GPL v2.0"],
       maintainers: ["dwyl"],
-      links: %{"GitHub" => "https://github.com/dwyl/exbase58"}
+      links: %{"GitHub" => "https://github.com/dwyl/base58"}
     ]
   end
 end
