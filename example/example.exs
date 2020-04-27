fdf7f23063db0526ca513cb0048258ccff2c3b3d
SimonLab <simon.labondance@gmail.com>
2020-04-27 13:03:48 +0100

    fix example and use to_charlist, #17

--- example/example.exs
+++ example/example.exs
@@ -1,8 +1,10 @@
 defmodule Example do
 
   hello = "hello"
-  helloBase58 = Base58Encode.encode(hello)
+  helloBase58 = Base58.encode(hello)
+  helloBase58Decode = Base58.decode(helloBase58)
 
   IO.puts("the \"#{hello}\" binary is represented as \"#{helloBase58}\" in base58")
+  IO.puts("the \"#{helloBase58Decode}\" decoded is #{hello})")
 
 end
